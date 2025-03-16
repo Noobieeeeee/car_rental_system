10eb06071d7585cfb53e16495eca731039e7662f
Xyrus <dominguez.xyrusvincent1@gmail.com>
2025-03-16 20:22:33 +0800

    new development changes

--- car_rental_system/app.py
+++ car_rental_system/app.py
@@ -1,10 +1,13 @@
-from flask import Flask
+from flask import Flask, render_template
 
+# Initialize the Flask app
 app = Flask(__name__)
 
+# Route for the landing page
 @app.route('/')
 def home():
-    return "Welcome to the Car Rental System!"
+    return render_template('mainpage.html')
 
+# Run the application
 if __name__ == '__main__':
     app.run(debug=True)
